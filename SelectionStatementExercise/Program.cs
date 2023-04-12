7f88ffe5c3bdb8f869f8dd082da610d721881a68
katherine Buendia <kbbuendia24@gmail.com>
2023-04-12 14:26:51 -0400

    If, else if excercise

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,56 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            int? favNum = 5;
+
+            Console.WriteLine(" Please guess my Favorite number");
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (favNum > userInput)
+            {
+                Console.WriteLine(" too low");
+            }
+            else if (favNum < userInput)
+            {
+                Console.WriteLine("too high");
+            }
+            else
+            {
+                Console.WriteLine(" good Try");
+
+
+
+                // second practice to see diff results
+
+
+
+                if (favNum == userInput)
+                {
+                    Console.WriteLine(" Great Guess! ");
+                }
+
+                else if (favNum < userInput)
+
+                {
+                    Console.WriteLine(" no  luck huh ? Try again !");
+
+                }
+
+                else if (favNum > userInput)
+
+                {
+                    Console.WriteLine(" one more try , Try again !");
+                }
+
+
+                else
+
+                    Console.WriteLine(" Good try !, The answer was 5. Play again !");
+
+
+
+            }
         }
     }
 }
